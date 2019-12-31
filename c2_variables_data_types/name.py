223e438101a3e01a92b9da9a61800c650816f2a3
paulo-sk <paulobsb1337@gmail.com>
2019-12-31 15:23:19 -0300

    strings e formataçao

---
+++ c2_variables_data_types/name.py
@@ -0,0 +1,12 @@
+# title() method = change the first letter of eache word to captial letters.
+
+name = "ada lovelace"
+print(name.title())
+print(name.lower())
+print(name.upper())
+
+# using variables in Strings
+first_name = "ada"
+second_name = "lovelace"
+full_name = f"{first_name} {second_name}"
+print(f"hello, {full_name.title()}")
