10b525a6c644138d8f2949e003e4e005a821f5cc
paulo-sk <paulobsb1337@gmail.com>
2020-01-01 04:03:26 -0300

    adicionando numeros e finalizando capitulo 2

--- c2_variables_data_types/numeros.py
+++ c2_variables_data_types/numeros.py
@@ -8,3 +8,9 @@ print(div_inteira)
 # underscore in numbers (facilita a leitura de numeros grandes)
 universe_age = 14_000_000_000
 print(universe_age) 
+
+# multiple assignment
+x, y, z = 0, 0, 0
+
+# constants
+MAX_CONNECTIONS = 500
