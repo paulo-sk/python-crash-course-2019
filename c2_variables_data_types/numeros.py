323817ed89b85d0599fcdf76e2be9a6ad7d3f207
paulo-sk <paulobsb1337@gmail.com>
2020-01-01 04:00:35 -0300

    adicionado numeros.py

---
+++ c2_variables_data_types/numeros.py
@@ -0,0 +1,10 @@
+potencia = 3 ** 2
+div = 3 / 2
+div_inteira = 3 // 2
+print(potencia)
+print(div)
+print(div_inteira)
+
+# underscore in numbers (facilita a leitura de numeros grandes)
+universe_age = 14_000_000_000
+print(universe_age) 
